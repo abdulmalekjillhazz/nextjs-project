08010fc0aa8223cf1123457bc03ed5b1a7b17ea7
abdulmalekjillhazz <abdulmalek67343367@gmail.com>
2025-11-13 14:11:22 +0600

    something is change

--- app/page.js
+++ app/page.js
@@ -5,51 +5,37 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-black text-white flex flex-col items-center justify-center px-6">
       {/* Navigation */}
-      <nav className="flex flex-wrap gap-6 text-lg font-medium mb-12">
-        <Link
-          href="/about"
-          className=" text-3xl no-underline text-white hover:text-gray-400 transition duration-200"
-        >
-          About
-        </Link>
-        <Link
-          href="/services"
-          className="no-underline text-white hover:text-gray-200 transition duration-200 text-3xl"
-        >
-          Services
-        </Link>
-        <Link
-          href="/blog"
-          className="no-underline text-white hover:text-gray-400 text-3xl transition duration-200"
-        >
-          Blog
-        </Link>
-      </nav>
+      <nav className ="absolute top-20 right-6 flex gap-30 text-4xl font-medium">
+  <Link href="/about" className ="no-underline text-white hover:text-gray-400 transition duration-200">About</Link>
+  <Link href="/services" className ="no-underline text-white hover:text-gray-400 transition duration-200">Services</Link>
+  <Link href="/blog" className ="no-underline text-white hover:text-gray-400 transition duration-200">Blog</Link>
+</nav>
+       
 
       {/* Hero Section */}
       <section className="text-center flex flex-col items-center gap-6">
-        <h1 className="text-4xl md:text-6xl font-bold">Welcome to My Website</h1>
+       
         <p className="text-gray-300 md:text-lg">
           Explore my services, milestones, and blogs below:
         </p>
 
         {/* Profile Image */}
-        <div className="py-6">
-          <Image
+        <div>
+          <Image  className =" w-70 h-70 rounded-full object-cover border-4  border-blue-300"
             src="/profile.png"
             alt="Profile Image"
-            width={150}
-            height={150}
-          />
+            width={500}
+            height={500}
+            />
         </div>
 
         <p className="text-white text-lg md:text-xl">Web Designer and Developer</p>
 
         {/* Additional Info Section */}
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left max-w-3xl mt-12">
-          <h2 className="text-2xl md:text-4xl font-semibold leading-snug">
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left max-w-2xl mt-12">
+          <p className=" text-2xl">
             Hello world! I created this website for experiments and projects.
-          </h2>
+          </p>
         </div>
       </section>
     </main>
